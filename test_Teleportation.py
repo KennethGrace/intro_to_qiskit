59ea9c1ed5c5d2ae63a58333db9870710dea9a86
Kenneth Grace <kenneth.grace@dyntek.com>
2020-01-02 15:47:55 -0800

    consolidated teleportation

--- test_Teleportation.py
+++ test_Teleportation.py
@@ -1,16 +1,18 @@
 # via Qiskit, we will write a Quantum Teleportation Algorithm. ALRIGHT, so, a qubit, q0, is set to some initial state, then the information in q0 is 'copied' to q2 (copying is impossible in quantum computing, so the information must be telported, this is what is meant by quantum teleportation). Pretty dope.
 # %% import qiskit library, as well as the plotting tools, and an exception hander
+import math
 from qiskit import *
 from qiskit.tools.visualization import plot_histogram
 from qiskit.tools.monitor import job_monitor
 from qiskit.providers.ibmq.job.exceptions import *
+# %% set wether to use Aer or IBMQ
+use_ibmq = False
 # %% build the quantum circuit
 circuit = QuantumCircuit(3, 3)
 # %% draw the circuit as initialized
 circuit.draw(output='mpl')
 # %% apply an initial state to q1, and draw
-circuit.x(0)
-circuit.h(1)
+circuit.ry(math.radians(90), 0)
 circuit.barrier()
 circuit.draw(output='mpl')
 # %% hadamard on q1 and conditional x on q2 based on q1
@@ -33,17 +35,21 @@ circuit.draw(output='mpl')
 # %% measure both registers for validation of teleportation, then draw
 circuit.measure([0, 1, 2], [0, 1, 2])
 circuit.draw(output='mpl')
-# %% run this circuit using IBMQ
-try:
-    IBMQ.load_account()
-    provider = IBMQ.get_provider('ibm-q')
-    qcomp = provider.get_backend('ibmq_16_melbourne')
-    job = execute(circuit, backend=qcomp, shots=2048)
-    job_monitor(job)
-except IBMQJobFailureError:
-    print('Job Failed with Below Error')
-    print(job.error_message())
-result = job.result()
+# %% run this circuit using the specified backend
+if use_ibmq:
+    try:
+        IBMQ.load_account()
+        provider = IBMQ.get_provider('ibm-q')
+        qcomp = provider.get_backend('ibmq_16_melbourne')
+        job = execute(circuit, backend=qcomp, shots=2048)
+        job_monitor(job)
+    except IBMQJobFailureError:
+        print('Job Failed with Below Error')
+        print(job.error_message())
+    result = job.result()
+else:
+    simulator = Aer.get_backend('qasm_simulator')
+    result = execute(circuit, backend=simulator, shots=2048).result()
 # %% print the raw results to a histogram
 counts = result.get_counts(circuit)
 plot_histogram(counts)
